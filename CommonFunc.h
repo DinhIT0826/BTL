b5d34daed27c710a25b1fdbfa97f9c0d2f2caf0a
DinhIT0826 <24020067@vnu.edu.vn>
2025-03-15 21:01:09 +0700

    add file CommonFunc.h

---
+++ CommonFunc.h
@@ -0,0 +1,26 @@
+
+#ifndef COMMON_FUNTION_H_
+#define COMMON_FUNTION_H_
+
+#include <windows.h>
+#include <string>
+#include <SDL>h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+
+static SDL_window* g_windows = NULL;
+static SDL_Renderer* g_screen = NULL;
+static SDL_Event* g_event;
+
+const int SCREEN_WIDTH = 1200;
+const int SCREEN_HEIGHT = 600;
+const int SCREEN_BPP = 32;
+
+const int COLOR_KEY_R = 167;
+const int COLOR_KEY_G = 640;
+const int COLOR_KEY_B = 180;
+
+
+
+#endif // COMMON_FUNTION_H_
