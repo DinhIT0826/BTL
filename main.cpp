6bf702bb07580e28cace04da43db6f477adf1229
DinhIT0826 <24020067@vnu.edu.vn>
2025-03-16 09:37:16 +0700

    add file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 
 #include "CommonFunc.h"
+#include "BaseObject.h"
 
 BaseObject g_background;
 
@@ -25,31 +26,65 @@ bool InitData()
     else
     {
         g_screen = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED);
-    if (g_screen == NULL)
-        success = false;
-    else
-    {
+        if (g_screen == NULL)
+            success = false;
+        else
+        {
         SDL_SetRenderDrawColor(g_screen, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR);
         int imgFlags = IMG_INIT_PNG;
         if (!(IMG_Init(imgFlags) && imgFlags))
             success = false;
+        }
     }
-    }
-
-return success;
+    return success;
 }
 
 bool LoadBackground()
 {
-    bool ret = g_background.LoadImg("img/background.png", g_screen);
+    bool ret = g_background.LoadImg("img//background.png", g_screen);
+
     if (!ret)
         return false;
 
     return true;
 }
+void close()
+{
+    g_background.Free();
 
+    SDL_DestroyRenderer(g_screen);
+    g_screen = NULL;
+
+    SDL_DestroyWindow(g_window);
+    g_window = NULL;
+
+    IMG_Quit();
+    SDL_Quit();
+}
 
 int main(int argc, char* argv[])
 {
+    if(InitData() == false) return -1;
+
+    if(LoadBackground() == false) return -1;
+
+    bool is_quit = false;
+    while(!is_quit)
+    {
+        while(SDL_PollEvent(&g_event) != 0)
+        {
+            if(g_event.type == SDL_QUIT)
+            {
+                is_quit = true;
+            }
+        }
+        SDL_SetRenderDrawColor(g_screen, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR);
+        SDL_RenderClear(g_screen);
+
+        g_background.Render(g_screen, NULL);
+
+        SDL_RenderPresent(g_screen);
+    }
+    close();
     return 0;
 }
