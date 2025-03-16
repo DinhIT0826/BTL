99c6d88c80c52beb272df6c727cf2c12f7e06b8e
DinhIT0826 <24020067@vnu.edu.vn>
2025-03-17 01:17:44 +0700

    add file

--- CommonFunc.h
+++ CommonFunc.h
@@ -25,4 +25,32 @@ const int COLOR_KEY_B = 180;
 
 const int RENDER_DRAW_COLOR = 0xff;
 
+#define BLANK_TILE 0
+#define TILE_SIZE 64
+
+#define MAX_MAP_X 400
+#define MAX_MAP_Y 10
+
+typedef struct Input
+{
+    int left_;
+    int right_;
+    int up_;
+    int down_;
+    int jump_;
+};
+
+
+typedef struct Map
+{
+    int start_x_;
+    int start_y_;
+
+    int max_x_;
+    int max_y_;
+
+    int tile[MAX_MAP_Y][MAX_MAP_X];
+    char* file_name_;
+};
+
 #endif // COMMON_FUNTION_H_
